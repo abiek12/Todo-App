fadbb656a0166adb06364945c37b470e98bd77c0
Abhishek p p <abhishekkanichery@gmail.com>
2024-09-24 08:33:17 +0530

    added a clean script for the clean up the previous build files

---
+++ server/dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 5000;
+// Middlewares
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send("Welcome to Backend");
+});
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
+// Database Connection
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
-console.log("Hii")
+import express, { Application } from 'express'
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+
+app.get('/',(req, res)=> {
+    res.send("Welcome to Backend");
+})
+
+app.listen(PORT, ()=> console.log(`Server Running on Port ${PORT}`))
+
+// Database Connection
