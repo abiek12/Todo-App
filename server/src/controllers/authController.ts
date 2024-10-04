585f1fb2612bd4e155c7a86ae1d42d94d6f776d3
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-04 22:24:36 +0530

    added user login controllers

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,35 +3,59 @@ import { commonMessages, httpStatus } from "../utils/common";
 import { UserRepository } from "../models/repositories/userRepository";
 import { AuthServices } from "../services/authServices";
 
-const statusCode = new httpStatus
-const messages = new commonMessages
-const userRepo = new UserRepository();
-const authServices = new AuthServices();
+export class authControllers {
+    private statusCode = new httpStatus
+    private messages = new commonMessages
+    private userRepo = new UserRepository();
+    private authServices = new AuthServices();
 
-export const registration = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    try {
-        if(!email || !password) {
-            console.error("Mandatatory fields are missing!")
-            res.status(statusCode.BAD_REQUEST).send("Mandatory fields are missing!");
+    registration = async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        try {
+            if(!email || !password) {
+                console.error("Mandatatory fields are missing!")
+                res.status(this.statusCode.BAD_REQUEST).send("Mandatory fields are missing!");
+            }
+
+            const exisitingUser = await this.userRepo.findUserByEmail(email);
+            if(exisitingUser) {
+                console.error("User Already Exist!");
+                res.status(this.statusCode.BAD_REQUEST).send("User Already Exist!");
+            }
+
+            const newUser = await this.authServices.userRegistration(email, password);
+            const msg = {
+                message: "User Registered Successfully.",
+                newUser
+            }
+            console.log("User Registered Successfully.");
+            res.status(this.statusCode.SUCCESS).send(msg)
+
+        } catch (error) {
+            console.error("Error Registering User");
+            res.status(this.statusCode.INTERNAL_ERROR).send(this.messages.INTERNAL_ERORR);
         }
+    }
 
-        const exisitingUser = await userRepo.findUserByEmail(email);
-        if(exisitingUser) {
-            console.error("User Already Exist!");
-            res.status(statusCode.BAD_REQUEST).send("User Already Exist!");
+    login = async (req: Request, res: Response) => {
+        const {email, password} = req.body;
+        try {
+            if(!email || !password) {
+                console.error("Mandatatory fields are missing!")
+                res.status(this.statusCode.BAD_REQUEST).send("Mandatory fields are missing!");
+            }
+
+            const user = await this.authServices.userLogin(email, password);
+            if(!user) {
+                console.error("User doesn't exist!")
+                res.status(this.statusCode.NOT_FOUND).send("User doesn't exist!");
+            }
+
+            console.log("User Login Successfully");
+            res.status(this.statusCode.SUCCESS).json({message:'Login Successful', user});
+        } catch (error) {
+            console.error("Error Registering User");
+            res.status(this.statusCode.INTERNAL_ERROR).send(this.messages.INTERNAL_ERORR);
         }
-
-        const newUser = await authServices.userRegistration(email, password);
-        const msg = {
-            message: "User Registered Successfully.",
-            newUser
-        }
-        console.log("User Registered Successfully.");
-        res.status(statusCode.SUCCESS).send(msg)
-
-    } catch (error) {
-        console.error("Error Registering User");
-        res.status(statusCode.INTERNAL_ERROR).send(messages.INTERNAL_ERORR);
     }
 }
