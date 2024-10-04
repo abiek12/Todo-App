0c2d43c936e8ca1b1c2f270ab02cf8b76d7f27b9
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-04 16:06:49 +0530

    added user registration and user login services

--- server/src/models/interfaces/IUser.ts
+++ server/src/models/interfaces/IUser.ts
@@ -1,7 +1,7 @@
 export interface IUser {
-    _id: string;
+    _id?: string;
     email: string;
     password: string;
-    createdAt: Date;
-    updatedAt: Date;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
---
+++ server/src/services/authServices.ts
@@ -0,0 +1,27 @@
+import { IUser } from "../models/interfaces/IUser";
+import { UserRepository } from "../models/repositories/userRepository";
+import { comparePassword, hashPassword } from "../utils/passwordUtils";
+
+export class AuthServices {
+    private userRepo = new UserRepository();
+
+    userRegistration = async (email: string, password: string): Promise<IUser> => {
+        const hashedPassword = await hashPassword(password);
+        const user = {
+            email,
+            password: hashedPassword
+        }
+        const newUser = await this.userRepo.createUser(user);
+        return newUser;
+    }
+
+    userLogin = async (email: string, password: string): Promise<IUser | null> => {
+        const user = await this.userRepo.findUserByEmail(email);
+        if(!user) return null;
+
+        const isPasswordValid = await comparePassword(password, user.password);
+        if(!isPasswordValid) return null;
+
+        return user;
+    }
+}
