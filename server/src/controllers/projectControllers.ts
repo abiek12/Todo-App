86d6137f8a30e723ae5f60efd53d5e2ad356aabc
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-05 19:56:04 +0530

    fix controllers

--- server/src/controllers/projectControllers.ts
+++ server/src/controllers/projectControllers.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import { ProjectRepository } from "../models/repositories/projectRepository";
 import { commonMessages, httpStatus } from "../utils/common";
 import { todoRepository } from "../models/repositories/todoRepository";
+import { ProjectServices } from "../services/projectServices";
 
-export class ProjectServices {
-    private projectRepo = new ProjectRepository();
+export class ProjectControllers {
+    private projectService = new ProjectServices();
     private todoRepo = new todoRepository();
     private statusCode = new httpStatus;
     private messages = new commonMessages;
@@ -17,7 +18,7 @@ export class ProjectServices {
                 return res.status(this.statusCode.BAD_REQUEST).send("Mandatory fields are missing!");
             }
 
-            const newProject = await this.projectRepo.createProject(title);
+            const newProject = await this.projectService.createProject(title);
             if(newProject) {
                 const newTodo = await this.todoRepo.createTodo(todos);
                 newProject.todos.push(newTodo);
@@ -36,11 +37,11 @@ export class ProjectServices {
             const {_id} = req.query;
 
             if(_id) {
-                const project = await this.projectRepo.findProjectById(_id as string);
+                const project = await this.projectService.getSpecificProject(_id as string);
                 console.info("Project Fetched Successfully!");
                 return res.status(this.statusCode.SUCCESS).send(project);
             }
-            const allProjects = await this.projectRepo.findAllProject();
+            const allProjects = await this.projectService.getAllProject();
 
             console.info("Project Fetched Successfully!");
             return res.status(this.statusCode.SUCCESS).send(allProjects);
