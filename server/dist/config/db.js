75ea1e571bc6171b5f775e11029ae03c64e67c6f
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-05 15:47:38 +0530

    fix run script

--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -15,7 +15,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        console.log(process.env.MONGO_URI);
         yield mongoose_1.default.connect(process.env.MONGO_URI);
         console.log('MongoDB connected...');
     }
--- server/dist/server.js
+++ server/dist/server.js
@@ -6,11 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./config/db"));
+const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes_1.default);
+// app.use("/api/project", );
 app.get('/', (req, res) => {
     res.send("Welcome to Backend");
 });
