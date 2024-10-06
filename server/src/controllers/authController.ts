5107f1fd8c60c7f9d68b3a6fb38622e895f42fa6
Abhishek p p <abhishekkanichery@gmail.com>
2024-10-06 12:01:58 +0530

    generate the access token and refresh token while user login

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { commonMessages, httpStatus } from "../utils/common";
+import { commonMessages, generateAccessToken, generateRefreshToken, httpStatus } from "../utils/common";
 import { UserRepository } from "../models/repositories/userRepository";
 import { AuthServices } from "../services/authServices";
 
@@ -46,13 +46,16 @@ export class authControllers {
             }
 
             const user = await this.authServices.userLogin(email, password);
-            if(!user) {
+            if(!user || !user._id) {
                 console.error("User doesn't exist!")
                 return res.status(this.statusCode.NOT_FOUND).send("User doesn't exist!");
             }
 
+            const accesstoken: string = await generateAccessToken(user._id);
+            const refreshToken: string = await generateRefreshToken(user._id); 
+
             console.log("User Login Successfully");
-            return res.status(this.statusCode.SUCCESS).json({message:'Login Successful', user});
+            return res.status(this.statusCode.SUCCESS).json({message:'Login Successful', accesstoken, refreshToken});
         } catch (error) {
             console.error("Error Logging In User");
             return res.status(this.statusCode.INTERNAL_ERROR).send(this.messages.INTERNAL_ERORR);
